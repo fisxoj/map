5cdd9afd295cc04ab8c40b51ae90f84883e7879b
Matt Novenstern <fisxoj@gmail.com>
2012-11-16 14:19:29 -0600

    Finally add a domatrix macro

--- matrix/matrix-macros.lisp
+++ matrix/matrix-macros.lisp
@@ -1,29 +1,12 @@
 (in-package #:map)
-#|
-(defmacro loop-over-matrix (M indices &body body)
-  (let ((dim (array-dimensions M)) (index (car indices)))
-    (loop for index in indices
-	 for i from 0 by 1)
-    (if (not (cdr indices))
-	`(progn ,@body)
-	(loop for ,index from 0 upto (array-dimension))))
-  (loop
-     for var in indices
-     for i from 0 by 1
-       
-  ))
-|#
 
-(defmacro compose-matrix-loop (M indices &body body)
-  (inner-c-m-l M indices 0 body))
-
-(defmacro inner-c-m-l (M indices index &body body)
-  (if (not (= index (length indices)))
-      `(loop for ,(nth index indices) from 0 upto ,(array-dimension M index)
-	  do ,(inner-c-m-l M indices (1+ index) body))
-      `(progn ,@body)))
-#|
-(defmacro loop-over-vector (V index &body body)
-  `(loop for ,index from 0 upto ,(length V)
-      do (progn ,@body)))
-|#
+(defmacro domatrix ((element matrix &optional dimensions) &body body)
+  (declare (ignore dimensions))
+  (let ((i (gensym)))
+    `(loop for ,i from 0 upto (1- (array-total-size ,matrix))
+	  for ,element = (row-major-aref ,matrix ,i)
+	;; FIXME: Bind a list of indices to this array if a variable is provided
+	;; when ,indices
+	;; do (setf ,indices (indices-from-row-major ,matrix ,i))
+	  
+	do (progn ,@body))))
--- package.lisp
+++ package.lisp
@@ -17,6 +17,9 @@
    #:czeros
    #:mtrace
 
+   ;; Matrix macros
+   #:domatrix
+
    ;; Vector operations
    #:norm
 
