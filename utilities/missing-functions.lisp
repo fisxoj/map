aa06ac1d44cdb503e4b35e4468738618fe84822e
Matt Novenstern <fisxoj@gmail.com>
2012-11-27 20:58:10 -0600

    License code under the AGPL 3
    Chose the AGPL because of the high likelihood of scientific computing
    resources being available over terminals and whatnot.

--- utilities/missing-functions.lisp
+++ utilities/missing-functions.lisp
@@ -1,13 +1,26 @@
-(in-package :map)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; missing-functions.lisp
 ;;
-;; (c) 2012 Matt Novenstern <fisxoj@gmail.com>
+;; Copyright (c) 2012 Matt Novenstern <fisxoj@gmail.com>
 ;;
-;; This file is for things I come across that feel like they should be in the standard
-;; lisp library, but aren't.  They should be very much in the style of standard lisp
-;; functions.
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; This code is free software; you can redistribute it and/or
+;; modify it under the terms of the version 3 of
+;; the GNU Affero General Public License as published by
+;; the Free Software Foundation.
+;;
+;; This code is distributed in the hope that it will be useful,
+;; but without any warranty; without even the implied warranty of
+;; merchantability or fitness for a particular purpose. See the GNU
+;; Affero General Public License for more details.
+;;
+;; Version 3 of the GNU Affero General Public License is in the file
+;; LICENSE that was distributed with this file.
+;; If it is not present, you can access it from
+;; https://www.gnu.org/licenses/agpl.txt (until superseded by a newer
+;; version) or write to the Free Software Foundation, Inc., 59 Temple Place,
+;; Suite 330, Boston, MA 02111-1307 USA
+;;
+
+(in-package :map)
 
 (defmacro multf (place &optional (delta 1))
   (let ((del (gensym)))
--- utilities/printing.lisp
+++ utilities/printing.lisp
@@ -1,3 +1,25 @@
+;; printing.lisp
+;;
+;; Copyright (c) 2012 Matt Novenstern <fisxoj@gmail.com>
+;;
+;; This code is free software; you can redistribute it and/or
+;; modify it under the terms of the version 3 of
+;; the GNU Affero General Public License as published by
+;; the Free Software Foundation.
+;;
+;; This code is distributed in the hope that it will be useful,
+;; but without any warranty; without even the implied warranty of
+;; merchantability or fitness for a particular purpose. See the GNU
+;; Affero General Public License for more details.
+;;
+;; Version 3 of the GNU Affero General Public License is in the file
+;; LICENSE that was distributed with this file.
+;; If it is not present, you can access it from
+;; https://www.gnu.org/licenses/agpl.txt (until superseded by a newer
+;; version) or write to the Free Software Foundation, Inc., 59 Temple Place,
+;; Suite 330, Boston, MA 02111-1307 USA
+;;
+
 (in-package #:map)
 
 (defmacro mute (&body body)
