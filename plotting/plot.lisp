13359b69c97430c7c5c74f06d10ffe86c488ad6b
Matt Novenstern <fisxoj@gmail.com>
2013-01-20 22:44:26 -0600

    Completely revamp plotting
    Hooray! long overdue and needs explaining in the documentation

--- plotting/plot.lisp
+++ plotting/plot.lisp
@@ -25,15 +25,19 @@
 (defparameter *gnuplot-path* #p"/usr/bin/gnuplot")
 (defparameter *gnuplot-args* '("-persist"))
 (defparameter *gnuplot-process* nil)
+;; Thanks http://yujianzhang.blogspot.com/2007/12/nicer-fonts-for-gnuplot.html
+(defparameter *gnuplot-font* "/usr/share/fonts/bitstream-vera/Vera.ttf,12")
 
-(defmacro with-plot (stream &body body)
+(defmacro with-plot ((stream &optional terminal pathspec) &body body)
+  (when (and (member terminal '(:eps :ps :png :jpg))
+	     (not pathspec))
+    (error "Attempting to plot to a file with no pathspec given"))
   `(let ((gnuplot (ensure-gnuplot-connection)))
-     (unwind-protect
-	  (with-open-stream (,stream (external-program:process-input-stream gnuplot))
-	    (format ,stream "set term x11~%")
-	    ,@body)
-;       (external-program:process-close gnuplot)
-)))
+     (with-open-stream (,stream (external-program:process-input-stream gnuplot))
+					;with-open-file (,stream #p"./test" :direction :output :if-exists :supersede)
+       (format ,stream "set term ~a font ~S~%" (string-downcase ,terminal) *gnuplot-font*)
+       (when ,pathspec (format ,stream "set output ~S~%" (namestring ,pathspec)))
+       ,@body)))
 
 (defun close-gnuplot-connection ()
   (when *gnuplot-process*
@@ -46,24 +50,21 @@
 				    :input :stream))
       *gnuplot-process*))
 
-(defun parse-style (style-alist)
-  (with-output-to-string (s)
-    ;; Do we have a title?  Print it first
-    (when (assoc :title style-alist)
-      (format s " title ~S" (cadr (assoc :title style-alist)))
-      (setf style-alist (remove-if (lambda (k) (eq (car k) :title)) style-alist)))
+(defun parse-style (stream style-number args)
+  ;; Do we have a title?  Print it first
 
-    ;; More style directives?  They live together
-    (when style-alist
-      (format s " with ")
-      (loop for (key value) in style-alist
-	 do (cond
-	      ((eq key :line-style)
-	       (format s "~a" (cond
-			      ((eq value :line) "line")
-			      ((eq value :line-points) "linespoints"))))
-	      ((eq key :yerr)
-	       (format s " yerr ")))))))
+  (format stream "set style line ~d " style-number)
+  ;; More style directives?  They live together
+
+  (if args
+      (destructuring-bind (&key line-type line-color line-width point-type point-size &allow-other-keys) args
+	(when line-type (format stream "lt ~a " line-type))
+	(when line-color (format stream "lc ~a " line-color))
+	(when line-width (format stream "lw ~a " line-width))
+	(when point-type (format stream "pt ~a " point-type))
+	(when point-size (format stream "ps ~a " point-size))
+	(format stream "~%"))
+      (format stream "default~%")))
 
 (defun print-vectors-to-stream (stream x y)
 	  (loop for xi across x
@@ -71,33 +72,60 @@
 	     do (format stream "~1,8f ~1,8f~%" xi yi))
 	  (format stream "e~%"))
 
-(defun plot (&rest args)
-  ;; Let's say things are (x y title) for now
-  (with-plot stream
-    (if (consp (car args))
+(defun plot (plot-sexps &key title (terminal :wxt) (pathspec "") x-label y-label)
+  ;; Make sure args is always a list of plot specs
+  (let ((plot-sexps (if (consp (car plot-sexps))
+			plot-sexps
+			(list plot-sexps))))
 
-	;; List of things to plot
-	(progn
-	  (loop for plot in (car args)
-	     do (format stream "plot '-' using 1:2 ~a" (parse-style (third plot)))
-	     unless (eql plot (car (last (car args))))
-	     do (format stream ","))
-	       
-	  (loop for plot in (car args)
-	     do (destructuring-bind (x y nil) plot
-		  (print-vectors-to-stream stream x y)))) ; hah! second-class functions to the rescue!
-	
+    (with-plot (stream terminal pathspec)
+       (when title (format stream "set title ~S~%" title))
+       (when x-label (format stream "set xlabel ~S~%" x-label))
+       (when y-label (format stream "set ylabel ~S~%" y-label))
 
-	;; One thing to plot, yay!
-	(let ((x (first args))
-	      (y (second args))
-	      (style (parse-style (third args))))
-	  (format stream "plot '-' using 1:2 ~a~%" style)
-	  (print-vectors-to-stream stream x y)))))
+      ;; Write line styles to stream
+      (loop
+	 for plot in plot-sexps
+	 for number from 1 by 1
+	 do (parse-style stream number (subseq plot 2)))
 
-(defun image (data)
-  (with-plot s
+      ;; Write plot commands to stream
+      (loop
+	 for plot in plot-sexps
+	 for number from 1 by 1
+	 initially (princ "plot " stream)
+	 do (format stream
+		    "'-' using 1:2 ls ~d title ~S with ~a"
+		    number
+		    (or (getf plot :title) "")
+		    (or (case (getf plot :line-style)
+			  (:lines "lines")
+			  (:points "points")
+			  (:lines-points "linespoints")
+			  (:impulses "impulses")
+			  (:dots "dots")
+			  (:steps "steps")
+			  ;; FIXME: Add others once we can parse more vectors (errorbars)
+			  (t "lines"))))
+	 unless (= (length plot-sexps) number)
+	 do (princ ", " stream)
+	 finally (princ #\Newline stream))
+
+      
+      (loop
+	 for plot in plot-sexps
+	 do (destructuring-bind (x y) (subseq plot 0 2)
+	      ;; hah! second-class functions to the rescue!
+	      (print-vectors-to-stream stream x y))))))
+
+(defun image (data &key title (terminal :wxt) (pathspec "") x-label y-label)
+  (with-plot (s terminal pathspec)
+    (when title (format s "set title ~S~%" title))
+    (when x-label (format s "set xlabel ~S~%" x-label))
+    (when y-label (format s "set ylabel ~S~%" y-label))
+
     (format s "set pm3d map~%")
+    (format s "set size ratio 1~%")
     (format s "splot '-' matrix~%")
     (loop for i from (1- (array-dimension data 1)) downto 0
        do (loop for j from 0 upto (1- (array-dimension data 0))
@@ -105,14 +133,19 @@
        do (format s "~%"))
     (format s "e~%")))
 
-(defun plot3d (data)
+(defun plot3d (data &key title (terminal :wxt) (pathspec "") x-label y-label z-label)
 ;  (declare (type real-matrix data))
-  (with-plot s
+  (with-plot (s terminal pathspec)
+    (when title (format s "set title ~S~%" title))
+    (when x-label (format s "set xlabel ~S~%" x-label))
+    (when y-label (format s "set ylabel ~S~%" y-label))
+    (when z-label (format s "set zlabel ~S~%" z-label))
+
 ;    (format s "set pm3d~%")
     (format s "set isosample 4~%")
-    (format s "splot '-'~%")
-    (loop for i from 0 upto (1- (array-dimension data 0))
-	 do (loop for j from 0 upto (1- (array-dimension data 1))
-		 do (format s "~g " (aref data i j)))
-	 do (format s "~%"))
+    (format s "splot '-' matrix~%")
+    (loop for i from 0 below (array-dimension data 0)
+       do (loop for j from 0 below (array-dimension data 1)
+	     do (format s "~g " (aref data i j)))
+       do (format s "~%"))
     (format s "e~%")))
